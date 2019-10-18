19a8ebf2a4f20902c10f4b18ccef9bf3b8ffff09
Kevin O Keeffe <kevokeeffe@gmail.com>
2019-10-18 20:11:51 +0100

    Updated user model for mongodb

--- models/users.js
+++ models/users.js
@@ -1,20 +1,36 @@
-const users=[
-    {
-        id:200001,
-        fName:'kevin',
-        lName:'o keeffe',
-        email:'kevokeeffe@gmail.com',
-        password:'123456',
-        permission:"admin"
-    },
-    {
-        id:200002,
-        fName:'peter',
-        lName:'griffen',
-        email:'petergriffen@gmail.com',
-        password:'123456',
-        permission:"average"
-    }
-];
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
 
-module.exports = users;
+let OrderSchema = new Schema({
+
+        fName: String,
+        lName: String,
+        email: String,
+        password: String,
+        permission: String,
+    },
+
+    { collection: 'users' });
+
+module.exports = mongoose.model('User', OrderSchema);
+
+//const users=[
+//     {
+//         id:200001,
+//         fName:'kevin',
+//         lName:'o keeffe',
+//         email:'kevokeeffe@gmail.com',
+//         password:'123456',
+//         permission:"admin"
+//     },
+//     {
+//         id:200002,
+//         fName:'peter',
+//         lName:'griffen',
+//         email:'petergriffen@gmail.com',
+//         password:'123456',
+//         permission:"average"
+//     }
+// ];
+//
+// module.exports = users;
