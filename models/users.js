fcb9076faa1c119cc921feb76276e35f9027bdae
Kevin O Keeffe <kevokeeffe@gmail.com>
2019-10-22 22:38:02 +0100

    e-mail identification added

--- models/users.js
+++ models/users.js
@@ -5,9 +5,9 @@ let OrderSchema = new Schema({
         _id: mongoose.Schema.Types.ObjectId,
         fName: {type: String},
         lName: {type: String},
-        email: {type: String, required: true, unique: true},
+        email: {type: String, required: true, unique: true, match:/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
         //unique does not validate the field it just optimises the field to be searched and indexed. Gives performance optimization
-
+        // match with a regex, checks to see if the email is of a valid type
         password: {type: String, required: true},
         permission: {type: String},
         active: {type: Boolean},
