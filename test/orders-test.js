9a3d75b99f1e9d38da9637f0c8c267dd40e791b2
Kevin O Keeffe <kevokeeffe@gmail.com>
2019-10-24 19:13:05 +0100

    First test working Get /order (order/all) endpoint

--- test/orders-test.js
+++ test/orders-test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const server = require("../../../bin/www");
+const server = require("../bin/www");
 const orders = require("../models/orders");
 const request = require("supertest");
 const _ = require("lodash");
@@ -8,7 +8,18 @@ const _ = require("lodash");
 describe('Orders', () => {
 
     describe("GET /order", () => {
-
+        it("should return all the donations", done => {
+            request(server)
+                .get("/order/all")
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .end((err, res) => {
+                    expect(res.body).to.be.a("array");
+                    //expect(res.body.length).to.equal(2);
+                    done(err);
+                });
+        });
     });
 
     describe("ADD /order", () => {
