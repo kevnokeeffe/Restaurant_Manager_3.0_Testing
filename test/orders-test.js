65a6e4d649a825f64ad02ddfb9429879bcb6df3f
Kevin O Keeffe <kevokeeffe@gmail.com>
2019-10-24 19:39:10 +0100

    Get all orders test compleated

--- test/orders-test.js
+++ test/orders-test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 const server = require("../bin/www");
-const orders = require("../models/orders");
+const order = require("../models/orders");
 const request = require("supertest");
 const _ = require("lodash");
 
@@ -8,17 +8,40 @@ const _ = require("lodash");
 describe('Orders', () => {
 
     describe("GET /order", () => {
-        it("should return all the donations", done => {
-            request(server)
-                .get("/order/all")
-                .set("Accept", "application/json")
-                .expect("Content-Type", /json/)
-                .expect(200)
-                .end((err, res) => {
-                    expect(res.body).to.be.a("array");
-                    //expect(res.body.length).to.equal(2);
-                    done(err);
-                });
+        it("should return all the orders", done => {
+            try {
+                request(server)
+                    .get("/order/all")
+                    .set("Accept", "application/json")
+                    .expect("Content-Type", /json/)
+                    .expect(200)
+                    .end((err, res) => {
+                        try {
+                            expect(res.body).to.be.a("array");
+                            expect(res.body.length).to.be.at.least(1);
+                            expect(res.body.length).to.be.at.most(9999);
+                            let value = _.map(res.body, order => {
+                                return {
+                                    main: order.main,
+                                    desert: order.desert
+                                };
+                            });
+                            expect(value).to.deep.include({
+                                main: "chicken goujons",
+                                desert: "cheesecake"
+                            });
+                            done();
+                        }catch (err) {
+                            done(err)
+                        }
+                    });
+            }catch(err){
+                    console.log(err);
+                    res.status(500).json({
+                        message: "Get Order did not work!",
+                        error: err
+                    });
+            }
         });
     });
 
