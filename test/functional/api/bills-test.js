f1a578c8d29a3076c788b817faa77f67d4d605b1
Kevin O'Keeffe <kevokeeffe@gmail.com>
2019-11-22 14:22:01 +0000

    Orders and Bills tests refactored to test mongodb Atlas DB, CLOUD

--- test/functional/api/bills-test.js
+++ test/functional/api/bills-test.js
@@ -2,32 +2,19 @@ const expect = require('chai').expect;
 const Order = require("../../../models/orders");
 const request = require("supertest");
 const _ = require("lodash");
-const MongoMemoryServer = require("mongodb-memory-server").MongoMemoryServer;
-const mongoose = require("mongoose");
-
-
+const mongoose = require('../../../db/mongoose');
+const {databasePassword, databaseUsername} = require('../../../config');
 
 let server;
-let mongod;
 let db, validID, validID2;
 
 describe('Bill', () => {
     before(async () => {
         try {
-            mongod = new MongoMemoryServer({
-                instance: {
-                    port: 27017,
-                    dbPath: "./test/database",
-                    dbName: "restaurantManager" // by default generate random dbName
-                }
-            });
-            // Async Trick - this ensures the database is created before
-            // we try to connect to it or start the server
-            await mongod.getConnectionString();
-
-            mongoose.connect("mongodb://localhost:27017/restaurantManager", {
+            mongoose.connect(`mongodb+srv://${databaseUsername}:${databasePassword}@cluster0-r3fv1.mongodb.net/restaurantManager?retryWrites=true&w=majority`, {
                 useNewUrlParser: true,
-                useUnifiedTopology: true
+                useUnifiedTopology: true,
+                useFindAndModify: false
             });
             server = require("../../../bin/www");
             db = mongoose.connection;
@@ -39,8 +26,6 @@ describe('Bill', () => {
     after(async () => {
         try {
             await db.dropDatabase();
-            await mongod.stop()
-            await server.close()
         } catch (error) {
             console.log(error);
         }
--- test/functional/api/orders-test.js
+++ test/functional/api/orders-test.js
@@ -1,30 +1,16 @@
 const expect = require('chai').expect;
-//const server = require("../bin/www");
 const Order = require("../../../models/orders");
 const request = require("supertest");
 const _ = require("lodash");
-const MongoMemoryServer = require("mongodb-memory-server").MongoMemoryServer;
-const mongoose = require("mongoose");
-
+const mongoose = require('../../../db/mongoose');
 let server;
-let mongod;
 let db, validID, validID2, validID3;
+const {databasePassword, databaseUsername} = require('../../../config');
 
 describe('Orders', () => {
     before(async () => {
         try {
-            mongod = new MongoMemoryServer({
-                instance: {
-                    port: 27017,
-                    dbPath: "./test/database",
-                    dbName: "restaurantManager" // by default generate random dbName
-                }
-            });
-            // Async Trick - this ensures the database is created before
-            // we try to connect to it or start the server
-            await mongod.getConnectionString();
-
-            mongoose.connect("mongodb://localhost:27017/restaurantManager", {
+            mongoose.connect(`mongodb+srv://${databaseUsername}:${databasePassword}@cluster0-r3fv1.mongodb.net/restaurantManager?retryWrites=true&w=majority`, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
                 useFindAndModify: false
@@ -39,8 +25,6 @@ describe('Orders', () => {
     after(async () => {
         try {
             await db.dropDatabase();
-            await mongod.stop();
-            await server.close()
         } catch (error) {
             console.log(error);
         }
