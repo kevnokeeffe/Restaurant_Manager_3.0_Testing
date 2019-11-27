124c63081fb5ba89d141a4645ce12a61429464f9
Kevin O'Keeffe <kevokeeffe@gmail.com>
2019-11-27 12:54:46 +0000

    esLint police fix

--- auth/VerifyToken.js
+++ auth/VerifyToken.js
@@ -5,18 +5,18 @@ let router = express.Router();
 
 //Token verification
 router.verifyToken = ((req, res, next) => {
-    const token = req.headers.authorization || req.headers['authenticate'];
-    if (!token)
-        return res.status(403).send({ auth: false, message: 'No token provided.' });
+	const token = req.headers.authorization || req.headers['authenticate'];
+	if (!token)
+		return res.status(403).send({ auth: false, message: 'No token provided.' });
 
-    jwt.verify(token, config.secret, function(err, decoded) {
-        if (err)
-            return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+	jwt.verify(token, config.secret, function(err, decoded) {
+		if (err)
+			return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
 
-        // if everything good, save to request for use in other routes
-        req.userId = decoded.id;
-        next();
-    });
+		// if everything good, save to request for use in other routes
+		req.userId = decoded.id;
+		next();
+	});
 });
 
 
--- auth/auth-service.js
+++ auth/auth-service.js
@@ -5,56 +5,56 @@ const config = require('../config');
 // Authentication methods
 
 router.requireLogin = (req, res, next) => {
-    const token= decodeToken(req);
-    if(!token){
-        return res.status(401).json({message: 'You must be logged in.'});
-    }
-    next();
-}
+	const token = decodeToken(req);
+	if (!token){
+		return res.status(401).json({message: 'You must be logged in.'});
+	}
+	next();
+};
 
 router.generateJWT = (user) => {
-    const tokenData = {fName: user.fName, id: user._id, email: user.email}
+	const tokenData = {fName: user.fName, id: user._id, email: user.email};
 
-    return jwt.sign({ user:tokenData }, config.secret, {
-        expiresIn: 86400 // expires in 24 hours
-    });
-}
+	return jwt.sign({ user: tokenData }, config.secret, {
+		expiresIn: 86400 // expires in 24 hours
+	});
+};
 
 router.decodeToken = (req) => {
-    const token = req.headers.authorization || req.headers
-        ['authenticate'];
-    if (!token){
-        return null;
-    }
-    try{
-        return jwt.verify(token, process.env.secret);
-    }catch (error){
-        return null;
-    }
-}
+	const token = req.headers.authorization || req.headers
+		['authenticate'];
+	if (!token){
+		return null;
+	}
+	try {
+		return jwt.verify(token, process.env.secret);
+	} catch (error){
+		return null;
+	}
+};
 
 router.getEmail= (req) => {
-    const token = decodeToken(req);
-    if(!token){
-        return null;
-    }
-    return token.user.email;
-}
+	const token = decodeToken(req);
+	if (!token){
+		return null;
+	}
+	return token.user.email;
+};
 
 router.getName= (req) => {
-    const token = decodeToken(req);
-    if(!token){
-        return null;
-    }
-    return token.user.fName;
-}
+	const token = decodeToken(req);
+	if (!token){
+		return null;
+	}
+	return token.user.fName;
+};
 
 router.getUserId = (req) => {
-    const token = decodeToken(req);
-    if(!token){
-        return null;
-    }
-    return token.user.id;
-}
+	const token = decodeToken(req);
+	if (!token){
+		return null;
+	}
+	return token.user.id;
+};
 
 module.exports = router;
