ea1b65a1d9ed131f25f24a84a5b1216c8403cef8
Kevin O'Keeffe <kevokeeffe@gmail.com>
2019-12-13 20:49:39 +0000

    .ENV Update

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
 module.exports = {
 	secret: 'g87y87n8gbblijy9y7567tv4dvif7mgjrb7nifj7hu6fbigngjfibfno6br67r57678798uyogbfv:::;::www',
-	databaseUsername: 'dbKevin',
-	databasePassword: 'KEV1984me',
-	mLabPassword: 'akakok1984'
 };
--- db/mongoose.js
+++ db/mongoose.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
 const {databasePassword, databaseUsername} = require('../config');
 mongoose.Promise = global.Promise;
-
+require('dotenv')
+    .config();
 //Local connection:
-//mongoose.connect('mongodb://localhost:27017/restaurantManager', { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost:27017/${process.env.MONGO_DB_NAME}', { useNewUrlParser: true });
 
 //mLab Connection:
-//`mongodb://${databaseUsername}:${mLabPassword}@ds241097.mlab.com:41097/heroku_q1g0hzrw`;
+//`mongodb://${process.env.MONGO_USER}:${process.env.MLAB_PASSWORD}@ds241097.mlab.com:41097/heroku_q1g0hzrw`;
 
 mongoose
   .connect(
-    `mongodb+srv://${databaseUsername}:${databasePassword}@cluster0-r3fv1.mongodb.net/restaurantManager?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-r3fv1.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`
     ,{ useUnifiedTopology: true, useNewUrlParser: true , useCreateIndex:true })
   .then(() => console.log('Successfully Connected to [ ' + db.name + ' ]'))
   .catch(err => console.log('Unable to Connect to [ ' + db.name + ' ]' + ' on orders route', err));
